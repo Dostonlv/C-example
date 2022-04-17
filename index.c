7b7ff8f00646f6c59bb237b6c9ac3b87c0c5d772
Doston <IamitachiUchixa@gmail.com>
2022-04-18 02:21:08 +0500

    first commit

---
+++ index.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h> 
+
+void hello(int n){
+    for(int i=0; i<n; i++){
+        printf("Hi bro!\n");
+    }
+    
+}
+
+
+int main(void){
+
+//Hello world!!!
+
+    // printf("Hello, World!\n");
+    // return 0;
+
+
+//   string name = get_string("What is your name?\n My name is: ");
+
+//  printf("Hello %s\n", name);
+
+// comparison
+
+//    int x,y;
+//    scanf("%d %d",&x,&y);
+//    if(x>y){
+//        printf("x is  than y\n ");
+//    }
+//    else if(x=y){
+//          printf("x is equal y\n");
+//    }else if(x<y){
+//          printf("y greater than x\n ");
+//    }
+
+// loops
+
+// while('true'){
+//     printf("Hello World!\n");
+// }
+
+// for(int i = 0; i<10; i++){
+//     printf("Hello World!\n");
+// }
+
+
+// string answer;
+// answer = get_string("What is your name?\n");
+// printf('hello, %s', answer);
+
+
+// practice test
+// int i = get_int("Integer: ");
+// printf("Hello, %i\n", i);
+
+// string n = get_string("What is your name?\n My name is: ");
+// int i = get_int("Your age: ");
+// printf(" Your name is: %s\n",n);
+// printf("Your age is: %i\n", i);
+
+// string username = get_string("Username: ");
+// string password = get_string("Password: ");
+// int age = get_int("Age: ");
+// printf(" Your username is: %s\n", username);
+// printf("Your password is: %s\n", password);
+// printf("Your age is: %i\n", age);
+
+// Arifmetika
+
+// float x = get_float("x: ");
+// float y = get_float("y: ");
+
+
+//for float
+
+
+// printf("x + y = %f\n", x+y);
+// printf("x - y = %f\n", x-y);
+// printf("x * y = %f\n", x*y);
+// printf("x / y = %f\n", x/y);
+
+// for int
+
+// printf("x+y = %i\n", x+y);
+// printf("x-y = %i\n", x-y);
+// printf("x*y = %i\n", x*y);
+// printf("x/y = %i\n", x/y);
+
+// odd or even
+
+// int x = get_int("x: ");
+
+// if(x % 2==0){
+//     printf("x is even\n");
+// }else{
+//     printf("x is odd\n");
+// }
+
+// Char
+
+// char c = get_char("Answer: ");
+
+// if(c=='Y'| c=='y'){
+//     printf("Yes\n");
+// }else if(c=='N'| c=='n'){
+//     printf("No\n");}
+
+// User About app
+
+// string username = get_string("Your name: ");
+// int age = get_int("Your age: ");
+// char isMarried = get_char("Are you married? (Y/N): ");
+// if(isMarried == 'y' | isMarried == 'Y'){
+//     printf("Your name is %s\n", username);
+//     printf("Your age is %i\n", age);
+//     printf("You are married: True\n");
+// }else if(isMarried == 'N' | isMarried == 'n'){
+//     printf("Your name is %s\n", username);
+//     printf("Your age is %i\n", age);
+//     printf("You are married: False\n");
+// }
+
+// sikl for + function
+
+
+hello(5);
+
+
+
+
+
+}
+
